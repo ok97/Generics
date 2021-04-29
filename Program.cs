79ef54037699341acf1476818efa221b5c3b108d
ok97 <omkhawshi0@gmail.com>
2021-04-29 16:23:42 +0530

    Added: Demo PrintArray class and define toPrint method

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 10, 20, 30 };
+            double[] doubleArr = { 10.15, 80.89, 30.69 };
+            char[] CharArr = { 'W', 'O', 'R', 'L','D' };
+
+            PrintArray.ToPrintInteger(intArr); //calling method
+            PrintArray.ToPrintdouble(doubleArr);//calling method
+            PrintArray.ToPrintCharachter(CharArr);//calling method
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
